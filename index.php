558cff825f4be5c3a96d9205a97b49911eab11c7
Lucas Navarro <108035760+Lucas-Navarro@users.noreply.github.com>
2025-04-13 23:45:15 -0300

    adicionando arquivos

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap" rel="stylesheet">
+    <title>Massas imperiais</title>
+</head>
+<body>
+    <header>
+        <img src="./imgs/Logo.png" alt="Logo massas imperiais">
+        <nav>
+            <a target="_blank" href="https://www.canva.com/design/DAE15Zm8aYk/2TOBdQOYtCYSxzm_WGnJRQ/view?utm_content=DAE15Zm8aYk&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton">Cardápio</a>
+            <p>/</p>
+            <a href="https://wa.me/5511940950891/?text=Olá vim pelo site, gostaria de fazer um pedido">Faça seu pedido</a>
+        </nav>
+    </header>
+
+    <main>
+        <section class="cards">
+            <section class="cards-card">
+                <img class="card-img" src="imgs/FOOD/marmita.jpeg">
+                <section class="card-botao-texto">
+                    <h3>Marmitas</h3>
+                    <a href="#Primeiro">Saiba mais</a>
+                </section>
+            </section>
+            <section class="cards-card">
+                <img class="card-img" src="imgs/FOOD/marmita.jpeg">
+                <section class="card-botao-texto">
+                    <h3>Salgados</h3>
+                    <a href="#Segundo">Saiba mais</a>
+                </section>
+            </section>
+            <section class="cards-card">
+                <img class="card-img" src="imgs/FOOD/marmita.jpeg">
+                <section class="card-botao-texto">
+                    <h3>Doces</h3>
+                    <a href="#Terceiro">Saiba mais</a>
+                </section>
+            </section>
+        </section>
+
+        <section class="textoDestaque">
+            <h1>Venha conhecer nosso estabelecimento!</h1>
+        </section>
+
+        <section class="caixa-imagens">
+            <section class="imagens">
+                <img src="imgs/imagemLoja.png" alt="Faixada da loja">
+            </section>
+        </section>
+        
+        <section class="caixa-mapa">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.966616090091!2d-46.3291573!3d-23.533703100000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce7af38b0f3719%3A0x6e27c6663bec1c83!2sAv.%20Conde%20de%20Monte%20Cristo%2C%20689%20-%20Jardim%20Monte%20Cristo%2C%20Suzano%20-%20SP%2C%2008670-020!5e0!3m2!1spt-BR!2sbr!4v1715559419091!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
+
+        <section class="caixa-horarios">
+            <section>
+                <h3>Seg a Sex</h3>
+                <p>09:00 as 18:00</p>
+                <h3>Sab</h3>
+                <p>09:00 as 18:00</p>
+            </section>
+            <img src="./imgs/Logo.png" alt="Logo massas imperiais">
+        </section>
+
+        
+
+        <section>
+            <a href="https://wa.me/5511940950891/?text=Olá vim pelo site, gostaria de fazer uma encomenda" class="icone-Flutuante"><img src="./imgs/whatsapp.png"></a>
+        </section>
+
+        <section class="cards-completos">
+            
+            <section class="card-completo">
+                <a name="Primeiro"></a>
+                <img src="./imgs/FOOD/marmita.jpeg">
+                <section>
+                    <h3>Test</h3>
+                    <p>
+                        Ao explorar novas fronteiras da tecnologia, 
+                        encontramos soluções inovadoras e desafios intrigantes. 
+                        A busca por conhecimento nunca cessa, impulsionada pela curiosidade 
+                        e pela paixão por descobrir o inexplorado. Em um mundo dinâmico, 
+                        adaptar-se é a chave, e cada desafio é uma oportunidade para crescimento 
+                        e evolução. Avançamos juntos, construindo o futuro com criatividade e 
+                        determinação.
+                    </p>
+                </section>
+            </section>
+            
+            <section class="card-completo">
+                <a name="Segundo"></a>
+                <img src="./imgs/FOOD/marmita.jpeg">
+                <section>
+                    <h3>Test</h3>
+                    <p>
+                        Ao explorar novas fronteiras da tecnologia, 
+                        encontramos soluções inovadoras e desafios intrigantes. 
+                        A busca por conhecimento nunca cessa, impulsionada pela curiosidade 
+                        e pela paixão por descobrir o inexplorado. Em um mundo dinâmico, 
+                        adaptar-se é a chave, e cada desafio é uma oportunidade para crescimento 
+                        e evolução. Avançamos juntos, construindo o futuro com criatividade e 
+                        determinação.
+                    </p>
+                </section>
+            </section>
+            
+            <section class="card-completo">
+                <a name="Terceiro"></a>
+                <img src="./imgs/FOOD/marmita.jpeg">
+                <section>
+                    <h3>Test</h3>
+                    <p>
+                        Ao explorar novas fronteiras da tecnologia, 
+                        encontramos soluções inovadoras e desafios intrigantes. 
+                        A busca por conhecimento nunca cessa, impulsionada pela curiosidade 
+                        e pela paixão por descobrir o inexplorado. Em um mundo dinâmico, 
+                        adaptar-se é a chave, e cada desafio é uma oportunidade para crescimento 
+                        e evolução. Avançamos juntos, construindo o futuro com criatividade e 
+                        determinação.
+                    </p>
+                </section>
+            </section>
+            
+            <section class="card-completo">
+                <a name="Quarto"></a>
+                <img src="./imgs/FOOD/marmita.jpeg">
+                <section>
+                    <h3>Test</h3>
+                    <p>
+                        Ao explorar novas fronteiras da tecnologia, 
+                        encontramos soluções inovadoras e desafios intrigantes. 
+                        A busca por conhecimento nunca cessa, impulsionada pela curiosidade 
+                        e pela paixão por descobrir o inexplorado. Em um mundo dinâmico, 
+                        adaptar-se é a chave, e cada desafio é uma oportunidade para crescimento 
+                        e evolução. Avançamos juntos, construindo o futuro com criatividade e 
+                        determinação.
+                    </p>
+                </section>
+            </section>
+        </section>
+        
+
+        <footer>
+            <section class="Logo-nome">
+                <img class="footer-logo" src="imgs/Logo.png" alt="Logo massas imperiais">
+                <h3>Massas imperiais</h3>
+            </section>
+
+            <section class="">
+                <p>Av. Conde de Monte Cristo, 689, Jardim Monte Cristo, SUZANO - SP</p>
+                <p>(11) 94095-089</p>
+            </section>
+           
+            <nav>
+                <a class="links-footer" target="_blank" href="https://www.instagram.com/massas_imperial/"><img class="icone-redes-sociais"  src="./imgs/instagram.png" alt="">instagram</a>
+                <a class="links-footer" target="_blank" href="https://wa.me/5511940950891/?text=Olá vim pelo site, gostaria de conhecer seus produtos."><img class="icone-redes-sociais"  src="./imgs/whatsapp.png" alt="">Whatsapp</a>
+            </nav>
+        </footer>
+    </main>
+</body>
+</html>
